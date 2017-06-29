d936415bf5228726f18c894006fd3fc9b9592efe
tirumalaprasad <flyingDRAGON08>
2017-06-29 02:51:59 -0500

    collectstatic

--- mysite/settings.py
+++ mysite/settings.py
@@ -124,7 +124,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-# username: admin
-# password: rubikscube
+
+# admin console credentials
+# user name: admin
+# email: admin@admin.com
+# password: simplekey
